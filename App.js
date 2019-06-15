91003ecbb8f2d7c4bb1135fc9e495a445259656e
rennanc <rennanchagas@outlook.com>
2019-06-15 20:01:01 -0300

    create initial structure of app

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import AddEntry from './components/AddEntry'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <AddEntry />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
