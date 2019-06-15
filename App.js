795a03469a732e3ab6452988c024edf824261720
rennanc <rennanchagas@outlook.com>
2019-06-15 20:58:03 -0300

    refresh code with layout

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import History from './components/History'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <AddEntry />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={{flex: 1}}>
+          <History />
+        </View>
+      </Provider>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
